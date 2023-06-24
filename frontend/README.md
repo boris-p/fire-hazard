6e1bb8ef2ad3c49042aa1cecaf4218d392f7054e
boris-p <3728381+boris-p@users.noreply.github.com>
2023-06-24 12:42:09 -0700

    using react map gl library

--- frontend/README.md
+++ frontend/README.md
@@ -9,4 +9,8 @@
 
 > npm run dev
 
-the app will be available at http://localhost:5173
+the app will be available at http://localhost:5173\
+
+## Tooling
+
+using [react-map-gl](https://visgl.github.io/react-map-gl/) for the mapgs
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
+import * as React from "react";
+import Map, { GeolocateControl } from "react-map-gl";
+
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <div>Fire-hazard App</div>
+      <h1>Fire-hazard App</h1>
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        mapboxAccessToken="pk.eyJ1IjoiYm9yLXBsIiwiYSI6ImNqangxenNvNTE1bWQzanAwNnRnOXU0ZWMifQ.xNWlg-CnhTvri40hwUlNdA"
+        style={{ width: "80vw", height: "80vh" }}
+        mapStyle="mapbox://styles/mapbox/satellite-v9"
+      >
+        <GeolocateControl />
+      </Map>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
